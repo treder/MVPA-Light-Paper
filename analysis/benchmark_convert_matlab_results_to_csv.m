a1f0a662f1b231533be0d2d0359ea299796eb62b
Matthias Treder <matthias.treder@gmail.com>
2019-11-18 11:14:36 +0000

    finished

--- analysis/benchmark_convert_matlab_results_to_csv.m
+++ analysis/benchmark_convert_matlab_results_to_csv.m
@@ -1,6 +1,6 @@
 
-%% MEG - Single subjects 
-load benchmarking_results_single_subjects.mat
+%% MEG - Single subjects [don't average across subjects]
+load benchmarking_results_single_subjects_matlab.mat
 
 nsubjects = 16;
 tmp = cell(nsubjects, 1);
@@ -13,8 +13,8 @@ for n=1:nsubjects
     tmp{n} = acc;
 end
 
-T = cat(1, tmp{:});
-writetable(T, 'benchmarking_results_single_subjects_acc.csv')
+T = mean(cat(3, tmp{:}),3);
+writetable(T, 'benchmarking_results_single_subjects_matlab_acc.csv')
 
 for n=1:nsubjects
     tim = table();
@@ -26,26 +26,63 @@ end
 
 T = cat(1, tmp{:});
 
-writetable(T, 'benchmarking_results_single_subjects_time.csv')
+writetable(T, 'benchmarking_results_single_subjects_matlab_time.csv')
 
-%% MEG - Single subjects 
+%% MEG - Single subjects [average across subjects]
+load benchmarking_results_single_subjects_matlab.mat
+
+nsubjects = 16;
+tmp = cell(nsubjects, 1);
+
+% add up accuracies
+acc = meg_acc{1};
+for n=2:nsubjects
+    for f = fieldnames(meg_acc{1})'
+        acc.(f{:}) = acc.(f{:}) + meg_acc{n}.(f{:});
+    end
+end
+% turn into table
+T= table();
+for f = fieldnames(meg_acc{1})'
+    T{:, f{:}} = acc.(f{:}) / nsubjects;
+end
+
+writetable(T, 'benchmarking_results_single_subjects_matlab_acc_average.csv')
+
+% add up times
+tim = meg_time{1};
+for n=2:nsubjects
+    for f = fieldnames(meg_time{1})'
+        tim.(f{:}) = tim.(f{:}) + meg_time{n}.(f{:});
+    end
+end
+% turn into table
+T= table();
+for f = fieldnames(meg_time{1})'
+    T{:, f{:}} = tim.(f{:}) / nsubjects;
+end
+
+writetable(T, 'benchmarking_results_single_subjects_matlab_time_average.csv')
+
+
+%% MEG super-subject
 clear
-load benchmarking_results_supersubject.mat
+load benchmarking_results_supersubject_matlab.mat
 
 T = table();
 for f = fieldnames(time)'
     T{:, f{:}}= time.(f{:});
 end
 
-writetable(T, 'benchmarking_results_supersubject.csv')
+writetable(T, 'benchmarking_results_supersubject_matlab.csv')
 
 %% fMRI 
 clear
-load benchmarking_results_haxby.mat
+load benchmarking_results_fmri_matlab.mat
 
 T = table();
 for f = fieldnames(time)'
     T{:, f{:}}= time.(f{:});
 end
 
-writetable(T, 'benchmarking_results_haxby.csv')
+writetable(T, 'benchmarking_results_fmri_matlab.csv')
